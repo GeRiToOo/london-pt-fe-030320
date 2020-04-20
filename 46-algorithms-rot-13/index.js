ac730f94c9aaf66c230ba26602cfc893aa6a22d2
GeRiToOo <57684252+GeRiToOo@users.noreply.github.com>
2020-04-20 13:55:31 +0100

    46

---
+++ 46-algorithms-rot-13/index.js
@@ -0,0 +1,38 @@
+let text = document.querySelector("textarea");
+let encrypt = document.querySelector("#encrypt");
+let decrypt = document.querySelector("#decrypt");
+let result = document.querySelector(".result");
+
+function rot13(string) {
+  let input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+  let index = x => input.indexOf(x);
+  let translate = x => (index(x) > -1 ? output[index(x)] : x);
+  return string
+    .split("")
+    .map(translate)
+    .join("");
+}
+
+function decryptRot13(string) {
+  let input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+  let index = x => output.indexOf(x);
+  let translate = x => (index(x) > -1 ? input[index(x)] : x);
+  return string
+    .split("")
+    .map(translate)
+    .join("");
+}
+
+encrypt.addEventListener("click", event => {
+  let textResult = rot13(text.value);
+
+  result.innerHTML = textResult;
+});
+
+decrypt.addEventListener("click", event => {
+  let textResult = decryptRot13(text.value);
+
+  result.innerHTML = textResult;
+});
